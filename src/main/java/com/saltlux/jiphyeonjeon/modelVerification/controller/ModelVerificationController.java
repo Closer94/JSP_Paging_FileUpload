86b61be83cd177a5bd8d8c5d4969ca090ae15208
admin <kabsung3@naver.com>
2022-07-28 17:06:21 +0900

    refactor: 프로젝트 구조 업로드

--- src/main/java/com/saltlux/jiphyeonjeon/modelVerification/controller/ModelVerificationController.java
+++ src/main/java/com/saltlux/jiphyeonjeon/modelVerification/controller/ModelVerificationController.java
@@ -1,14 +1,10 @@
 package com.saltlux.jiphyeonjeon.modelVerification.controller;
 
-import com.saltlux.jiphyeonjeon.modelVerification.service.ModelVerificationService;
 import com.saltlux.jiphyeonjeon.modelVerification.vo.ModelVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/main/java/com/saltlux/jiphyeonjeon/modelVerification/service/ModelVerificationService.java
+++
@@ -1,9 +0,0 @@
-package com.saltlux.jiphyeonjeon.modelVerification.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class ModelVerificationService {
-
-
-}
